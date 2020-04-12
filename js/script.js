320d7ab9c5d5fde22a036e39d905bfeb630bceb8
Verta-master <vertyagina@gmail.com>
2020-04-12 13:28:27 +0600

    Mobile

--- js/script.js
+++ js/script.js
@@ -11,13 +11,20 @@ if (window.innerWidth < 1230) {
   })
 }
 
+$('.footer__down').click(function() {
+  $(this).toggleClass('footer__down--opened');
+  $(this).next().slideToggle();
+})
+
 //Scroll to top
-$(window).scroll(function(){
-  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
-    $('.header').addClass('header--scroll');
-    $('main').addClass('main--scroll');
-  } else {
-    $('.header').removeClass('header--scroll');
-    $('main').removeClass('main--scroll');
-  };
-});
+if (window.innerWidth > 1230) {
+  $(window).scroll(function(){
+    if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+      $('.header').addClass('header--scroll');
+      $('main').addClass('main--scroll');
+    } else {
+      $('.header').removeClass('header--scroll');
+      $('main').removeClass('main--scroll');
+    }
+  })
+}
